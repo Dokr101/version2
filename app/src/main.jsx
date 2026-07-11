9cfc4693e6ef8928b3370096056c712e8ce3d686
Dokr101 <oggynishan4@gmail.com>
2026-07-11 21:09:06 +0100

    Fix React Router base path for Render

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/version2/app">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
